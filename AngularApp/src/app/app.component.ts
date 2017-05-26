7bc51a6dccf52135d8787aec86f846322a2cff07
devbratraghuvanshi <devbratcs0017@gmail.com>
2017-05-27 03:15:17 +0530

    component in templet

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
+    styles:[`
+  h1 
+  {
+    color:blue;
+  }
+  `]
 })
 export class AppComponent {
   name = 'Name Is Devbat';
